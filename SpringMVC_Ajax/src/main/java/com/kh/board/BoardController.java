c92f65a60136f2a7f31f825ccc0c407a3141ffb5
heechae96 <shinheechae@naver.com>
2023-04-02 23:43:35 +0900

    day0402 Qualifier 알아보기

--- SpringMVC_Ajax/src/main/java/com/kh/board/BoardController.java
+++ SpringMVC_Ajax/src/main/java/com/kh/board/BoardController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,7 +30,7 @@ public class BoardController {
 	private BoardService bService;
 	
 	@Autowired				// 의존성 주입
-	@Qualifier("fileUtil")	// 빈으로 등록
+//	@Qualifier("fileUtil")	// 빈으로 등록
 	private FileUtil fileUtil;
 
 	@GetMapping("/register")
