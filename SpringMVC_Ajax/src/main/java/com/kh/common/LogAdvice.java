f87f99260c91564d989c5f0064133397852b1bf7
heechae96 <shinheechae@naver.com>
2023-04-05 09:43:39 +0900

    day0405 commit
    change version

--- SpringMVC_Ajax/src/main/java/com/kh/common/LogAdvice.java
+++ SpringMVC_Ajax/src/main/java/com/kh/common/LogAdvice.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LogAdvice {
 	
+	// PointCut 어노테이션 pom.xml에 버전 이슈(1.9.0) 버전 사용 필요
 	@Pointcut("execution(* com.kh..*Impl.*(..))")
 	public void serviceImplPointCut() {
 		
